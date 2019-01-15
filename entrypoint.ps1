04a24ded0abade233441ac853ae34edc3769ae3b
Brandon Olin <brandon@devblackops.io>
2019-01-14 21:23:25 -0800

    wrap script in try/catch

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -8,20 +8,25 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
-# psake and PSDepend files
-if (-not $env:PSAKE_FILE)    {$env:PSAKE_FILE    = './psakeFile.ps1'}
-if (-not $env:PSDEPEND_FILE) {$env:PSDEPEND_FILE = './requirements.psd1'}
+try {
+    # psake and PSDepend files
+    if (-not $env:PSAKE_FILE)    {$env:PSAKE_FILE    = './psakeFile.ps1'}
+    if (-not $env:PSDEPEND_FILE) {$env:PSDEPEND_FILE = './requirements.psd1'}
 
-# Bootstrap dependencies...or not
-if (($env:SKIP_REQS -ne 'true' -and $env:SKIP_REQS -ne 1) -and (Test-Path -Path $env:PSDEPEND_FILE)) {
-    Invoke-PSDepend -Path $env:PSDEPEND_FILE -Install -Import -Force -WarningAction SilentlyContinue
-}
+    # Bootstrap dependencies...or not
+    if (($env:SKIP_REQS -ne 'true' -and $env:SKIP_REQS -ne 1) -and (Test-Path -Path $env:PSDEPEND_FILE)) {
+        Invoke-PSDepend -Path $env:PSDEPEND_FILE -Install -Import -Force -WarningAction SilentlyContinue
+    }
 
-# Execute psake task(s)
-if (Test-Path -Path $env:PSAKE_FILE) {
-    Invoke-psake -buildFile $env:PSAKE_FILE -taskList $Task -nologo
-    exit ([int](-not $psake.build_success))
-} else {
-    Write-Error "Could not find psake file [$env:PSAKE_FILE]"
+    # Execute psake task(s)
+    if (Test-Path -Path $env:PSAKE_FILE) {
+        Invoke-psake -buildFile $env:PSAKE_FILE -taskList $Task -nologo
+        exit ([int](-not $psake.build_success))
+    } else {
+        Write-Error "Could not find psake file [$env:PSAKE_FILE]"
+        exit 1
+    }
+} catch {
+    throw $_
     exit 1
 }
