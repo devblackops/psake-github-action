719ce1a9b1a83451b8e0d409b5e7633ef5d7e78b
Brandon Olin <brandon@devblackops.io>
2019-01-17 22:34:02 -0800

    Call BuildHelpers in test psake file to verify git works

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,9 +1,14 @@
 task default -depends Succeed
 
-task Succeed {
+task Init {
+    BuildHelpers\Set-BuildEnvironment -Force -WarningAction SilentlyContinue
+    Get-Item ENV:BH*
+}
+
+task Succeed -depends Init {
     'I succeeded'
 }
 
-task Fail {
+task Fail -depends Init {
     throw 'Oops, I did it again...'
 }
